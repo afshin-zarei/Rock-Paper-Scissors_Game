127f97e9cf52d0e7a586380a0011ad173bf546ef
Afshin Zarei <afshin.zarei2000@gmail.com>
2023-10-29 13:09:48 +0330

    updatecode

--- script/javascript.js
+++ script/javascript.js
@@ -1,25 +1,55 @@
-let userChoice = prompt("لطفا بین سنگ، کاغذ و قیچی یکی را انتخاب کنید!");
-console.log(`انتخاب شما: ${userChoice}`);
 const choices = ["سنگ", "کاغذ", "قیچی"];
-const randomNumber = Math.floor(Math.random() * 3);
-const computerChoice = choices[randomNumber];
-console.log(`انتخاب کامپیوتر: ${computerChoice}`);
-if (computerChoice === userChoice) {
-   console.log("شما مساوی شدید");
-} else if (userChoice === "سنگ") {
-   computerChoice === "قیچی"
-      ? console.log("شما برنده شدید")
-      : console.log("متاسفانه شما باختید");
-} else if (userChoice === "کاغذ") {
-   computerChoice === "سنگ"
-      ? console.log("شما برنده شدید")
-      : console.log("متاسفانه شما باختید");
-} else if (userChoice === "قیچی") {
-   computerChoice === "کاغذ"
-      ? console.log("شما برنده شدید")
-      : console.log("متاسفانه شما باختید");
-} else if (userChoice === "") {
-   console.log('لطفا یکی از کلمات "سنگ"، "کاغذ" یا "قیچی" را انتخاب کنید');
-} else {
-   console.log("لطفا کلمه صحیح را وارد کنید");
-}
+let playerScore = 0;
+let computerScore = 0;
+
+const checkWinner = (player, computer) => {
+   if (computer === player) {
+      return "مساوی";
+   } else if (player === "سنگ") {
+      return computer === "قیچی" ? "کاربر" : "کامپیوتر";
+   } else if (player === "کاغذ") {
+      return computer === "سنگ" ? "کاربر" : "کامپیوتر";
+   } else {
+      return computer === "کاغذ" ? "کاربر" : "کامپیوتر";
+   }
+};
+
+const showResult = (result) => {
+   if (result === "کاربر") {
+      console.log("شما بردید");
+      playerScore++;
+   } else if (result === "کامپیوتر") {
+      console.log("شما باختید");
+      computerScore++;
+   } else {
+      console.log("مساوی شدید");
+   }
+
+   console.log(`امتیاز شما: ${playerScore}`);
+   console.log(`امتیاز کامپیوتر: ${computerScore}`);
+   console.log("--------------------------");
+};
+
+const play = () => {
+   const playerChoice = prompt("لطفا بین سنگ، کاغذ و قیچی یکی را انتخاب کنید!");
+
+   if (choices.indexOf(playerChoice) !== -1) {
+      console.log(`انتخاب شما: ${playerChoice}`);
+   } else {
+      console.log('لطفا یکی از کلمات "سنگ"، "کاغذ" یا "قیچی" را انتخاب کنید');
+      return;
+   }
+
+   const randomNumber = Math.floor(Math.random() * choices.length);
+   const computerChoice = choices[randomNumber];
+   console.log(`انتخاب کامپیوتر: ${computerChoice}`);
+
+   const gameResult = checkWinner(playerChoice, computerChoice);
+   showResult(gameResult);
+
+   if (playerScore === 3) console.log("هورا! شما بردید");
+   else if (computerScore === 3) console.log("متاسفم شما باختید");
+
+   if (playerScore < 3 && computerScore < 3) play();
+};
+play();
